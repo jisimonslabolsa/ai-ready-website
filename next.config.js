203855c6db748517ed6486ca5e7dc5a7a949635c
jisimonslabolsa <joseignacio.simons@corp.hispavista.com>
2026-06-04 17:40:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*',
-        pathname: '/**',
-      },
-    ],
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
